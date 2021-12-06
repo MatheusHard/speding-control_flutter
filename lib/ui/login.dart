47630213807a3eed5dca30a70dade66c1f3ab88c
MatheusHard <matheushard2013@gmail.com>
2021-12-06 20:53:03 -0300

    Estilo Tela Login

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,3 +1,4 @@
+import 'package:control_speding_2/utils/core/app_text_styles.dart';
 import 'package:control_speding_2/utils/metods/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -30,11 +31,11 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+     /* appBar: AppBar(
         centerTitle: true,
         title: const Text("Controle de gastos f"),
         backgroundColor: Colors.blue,
-      ),
+      ),*/
       body:
 
 
@@ -47,10 +48,10 @@ class _LoginState extends State<Login> {
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                     colors: [
-                      Color(0xFF73AEF5),
-                      Color(0xFF61A4F1),
-                      Color(0xFF478DE0),
-                      Color(0xFF398AE5),
+                      Color(0xFFE5E6E4),
+                      Color(0xFFECECEB),
+                      Color(0xFFF2F3F2),
+                      Color(0xFFF9F9F8),
                     ],
                     stops: [0.1,0.4,0.7,0.9]
                 )
@@ -61,12 +62,17 @@ class _LoginState extends State<Login> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children:<Widget> [
-                  const Text("Login", style: TextStyle(
-                    color: Colors.blueAccent,
 
-                  ),),
+                  const Center(
+                    child: Text("Login", style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold
+
+                    ),),
+                  ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+                    padding: const EdgeInsets.fromLTRB(50, 20, 50, 30),
                     child: TextFormField(
                       controller: _user,
                       focusNode: _myFocusNode,
@@ -86,13 +92,85 @@ class _LoginState extends State<Login> {
                       },
                     ),
                   ),
-                ],
-              )),
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 50),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: _user,
+                      focusNode: _myFocusNode_2,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          labelText: "Senha:",
+                          hintText: "digite a senha",
+                          icon: Icon(Icons.perm_identity)
+                      ),
+                      validator: (value){
+                        if(value!.isEmpty || value == ""){
+                          _myFocusNode_2.requestFocus();
+                          //Utils.showDefaultSnackbar(context, );
+                          return "Insira a senha!!!";
+
+                        }
+                      },
+                    ),
+                  ),
+                  /*************BUTTON***************/
+
+    Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 10),
+    child: Center(
+    child: ElevatedButton(
+    onPressed: () {
+    /*if(_selectedFile != null)  {
+    if(_formKey.currentState.validate()) {
+    _cadastrarConta();
+    }
+    }else{
+    Utils.showDefaultSnackbar(_scaffoldKey, "Foto obrigatória!!! ");
+    }*/
+    },
+    style: ElevatedButton.styleFrom(
+    padding: EdgeInsets.zero,
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20))),
+    child: Ink(
+
+    decoration: BoxDecoration(
+    gradient: const LinearGradient(colors:
+    [
+      Color(0xFF57B6E5),
+      Color.fromRGBO(130, 87, 229, 0.695),
+    ]),
+    borderRadius: BorderRadius.circular(20)),
+    child:
+    Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    const Icon(Icons.save_rounded),
+    Container(
+    width: 150,
+    height: 50,
+    alignment: Alignment.center,
+    child:
+
+    Text(
+    'Acessar',
+    style: AppTextStyles.titleBold,
+    ),
+
+
+    ),
+    ],)))))
+   /***************FIM*****************/
+      ],)),
         ),
         ),
       ),
     );
   }
+
+
 
 
 }
